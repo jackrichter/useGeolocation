fc26c68d5630a797f43820984062b2b056933d3f
jackrichter <jackrichter1@gmail.com>
2024-04-19 01:48:31 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import { useState } from "react";
 import { useGeolocation } from "./useGeolocation";
 
-// function useGeolocation() {}
-
 export default function App() {
   const [countClicks, setCountClicks] = useState(0);
-  // const { lat, lng } = position;
 
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [position, setPosition] = useState({});
-  // const [error, setError] = useState(null);
-
-  // function getPosition() {
-  //   setCountClicks(count => count + 1);
-
-  //   if (!navigator.geolocation) return setError("Your browser does not support geolocation");
-
-  //   setIsLoading(true);
-  //   navigator.geolocation.getCurrentPosition(
-  //     pos => {
-  //       setPosition({
-  //         lat: pos.coords.latitude,
-  //         lng: pos.coords.longitude
-  //       });
-  //       setIsLoading(false);
-  //     },
-  //     error => {
-  //       setError(error.message);
-  //       setIsLoading(false);
-  //     }
-  //   );
-  // }
   const {
     isLoading,
     position: { lat, lng },
